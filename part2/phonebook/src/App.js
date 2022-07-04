e3239d472f646fd8fc60a623f63d9d9de1de70cf
Samuel Camilo Certuche Díaz <camilo.certuche@gmail.com>
2022-07-04 18:52:34 -0500

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,42 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const INITIAL_PERSONS = [{ name: "Arto Hellas", number: "040-123456" }];
+
+  const [persons, setPersons] = useState(INITIAL_PERSONS);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
-  const personExists = () => {
-    return persons.find(({ name }) => name === newName);
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
+
+  const nameExists = () => persons.find(({ name }) => name === newName);
+
+  const numberExists = () => persons.find(({ number }) => number === newNumber);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    let alreadyExists = false;
 
-    if (!personExists()) {
-      setPersons(persons.concat({ name: newName }));
-      setNewName("");
-    } else {
+    if (nameExists()) {
       alert(`${newName} is already added to phonebook`);
+      alreadyExists = true;
+    }
+
+    if (numberExists()) {
+      alert(`${newNumber} is already added to phonebook`);
+      alreadyExists = true;
+    }
+
+    if (!alreadyExists) {
+      setPersons(persons.concat({ name: newName, number: newNumber }));
+      setNewName("");
+      setNewNumber("");
     }
   };
 
@@ -31,12 +48,18 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      {persons.map(({ name }) => (
-        <p key={name}>{name}</p>
+      {persons.map(({ name, number }) => (
+        <p key={name}>
+          {name} {number}
+        </p>
       ))}
     </div>
   );
